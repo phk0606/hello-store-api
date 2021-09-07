867144e92f743c85bf92ec67cbc7ef6e4d06bc5f
hkpark <phk0606@naver.com>
2021-09-07 22:22:04 +0900

    상품 상세 페이지 수정

--- src/main/java/com/hellostore/ecommerce/dto/ShopProductDto.java
+++ src/main/java/com/hellostore/ecommerce/dto/ShopProductDto.java
@@ -2,7 +2,9 @@ package com.hellostore.ecommerce.dto;
 
 import com.hellostore.ecommerce.entity.ProductOption;
 import com.hellostore.ecommerce.enumType.ImageType;
+import com.hellostore.ecommerce.enumType.PointType;
 import com.hellostore.ecommerce.enumType.ProductShowType;
+import com.hellostore.ecommerce.enumType.ShippingFeeType;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +25,14 @@ public class ShopProductDto {
     private String productName;
     private int salePrice;
     private int regularPrice;
+    private PointType pointType;
+    private int pointPerPrice;
+    @Setter
+    private double point;
+    private ShippingFeeType shippingFeeType;
+    private int eachShippingFee;
+    @Setter
+    private double shippingFee;
     private String description;
     private Boolean newArrival;
     private Boolean best;
@@ -41,8 +51,10 @@ public class ShopProductDto {
     private long fileSize;
     private ImageType imageType;
 
-    private List<ProductOption> firstOptions = new ArrayList<>();
-    private List<ProductOption> secondOptions = new ArrayList<>();
+    @Setter
+    private List<ProductOptionDto> firstOptions = new ArrayList<>();
+    @Setter
+    private List<ProductOptionDto> secondOptions = new ArrayList<>();
 
     @Setter
     private List<byte[]> byteImages = new ArrayList<>();
@@ -69,12 +81,16 @@ public class ShopProductDto {
     }
 
     @QueryProjection
-    public ShopProductDto(Long productId, String productName, int salePrice, int regularPrice, String description, Boolean newArrival, Boolean best, Boolean discount, ProductShowType productShowType, String detailInfo, String shippingInfo, String exchangeReturnInfo) {
+    public ShopProductDto(Long productId, String productName, int salePrice, int regularPrice, PointType pointType, int pointPerPrice, ShippingFeeType shippingFeeType, int eachShippingFee, String description, Boolean newArrival, Boolean best, Boolean discount, ProductShowType productShowType, String detailInfo, String shippingInfo, String exchangeReturnInfo) {
 
         this.productId = productId;
         this.productName = productName;
         this.salePrice = salePrice;
         this.regularPrice = regularPrice;
+        this.pointType = pointType;
+        this.pointPerPrice = pointPerPrice;
+        this.shippingFeeType = shippingFeeType;
+        this.eachShippingFee = eachShippingFee;
         this.description = description;
         this.newArrival = newArrival;
         this.best = best;
--- src/main/java/com/hellostore/ecommerce/service/ShopProductService.java
+++ src/main/java/com/hellostore/ecommerce/service/ShopProductService.java
@@ -1,10 +1,15 @@
 package com.hellostore.ecommerce.service;
 
 import com.hellostore.ecommerce.dto.ProductListDto;
+import com.hellostore.ecommerce.dto.ProductOptionDto;
 import com.hellostore.ecommerce.dto.ProductSearchCondition;
 import com.hellostore.ecommerce.dto.ShopProductDto;
 import com.hellostore.ecommerce.entity.ProductImage;
+import com.hellostore.ecommerce.entity.ProductOption;
+import com.hellostore.ecommerce.enumType.PointType;
+import com.hellostore.ecommerce.enumType.ShippingFeeType;
 import com.hellostore.ecommerce.repository.ProductImageRepository;
+import com.hellostore.ecommerce.repository.ProductOptionRepository;
 import com.hellostore.ecommerce.repository.ShopProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +33,7 @@ public class ShopProductService {
 
     private final ShopProductRepository shopProductRepository;
     private final ProductImageRepository productImageRepository;
+    private final ProductOptionRepository productOptionRepository;
 
     public Page<ShopProductDto> getProductsPageCondition(
             ProductSearchCondition productSearchCondition, Pageable pageable) throws IOException {
@@ -49,6 +55,18 @@ public class ShopProductService {
     public ShopProductDto getProductById(Long productId) throws IOException {
         ShopProductDto product = shopProductRepository.getProductById(productId);
 
+        if (product.getPointType().equals(PointType.DEFAULT)) {
+            product.setPoint((product.getSalePrice() * 0.5) / 100);
+        } else if (product.getPointType().equals(PointType.EACH)) {
+            product.setPoint(product.getSalePrice() * product.getPointPerPrice());
+        }
+
+        if (product.getShippingFeeType().equals(ShippingFeeType.DEFAULT)) {
+            product.setShippingFee(2500);
+        } else if (product.getShippingFeeType().equals(ShippingFeeType.EACH)) {
+            product.setShippingFee(product.getEachShippingFee());
+        }
+
         List<ProductImage> productImages = productImageRepository.getProductDetailImages(productId);
 
         List<byte[]> byteImages = new ArrayList<>();
@@ -57,6 +75,26 @@ public class ShopProductService {
                     productImage.getFileName())));
         }
         product.setByteImages(byteImages);
+
+        List<ProductOption> productOptions1
+                = productOptionRepository.getProductOptions(productId, 1);
+
+        List<ProductOptionDto> productOptionDtos1 = new ArrayList<>();
+        for (ProductOption productOption : productOptions1) {
+            productOptionDtos1.add(new ProductOptionDto(productOption));
+        }
+
+        product.setFirstOptions(productOptionDtos1);
+
+        List<ProductOption> productOptions2
+                = productOptionRepository.getProductOptions(productId, 2);
+
+        List<ProductOptionDto> productOptionDtos2 = new ArrayList<>();
+        for (ProductOption productOption : productOptions2) {
+            productOptionDtos2.add(new ProductOptionDto(productOption));
+        }
+
+        product.setSecondOptions(productOptionDtos2);
 
         return product;
     }
